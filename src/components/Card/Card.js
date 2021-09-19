840f8319d8714bd0311565c667938b525e7749c2
Quan <hongquan03051997@gmail.com>
2021-09-18 22:31:29 -0700

    fix UI Add page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './Card.css';
 
 function Card(props){
+	if (props.currentPage){
+		return (
+			<div className='tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-5 pointer'>
+				<img alt="" src={props.img}/>
+				<h2>{props.name}</h2>
+			</div> 
+	)}
 	return (
-		<div className='tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-5 secret'>
-			<img alt="" src={props.img}/>
-			<h2>{props.name}</h2>
-		</div> 
-	);
+		<div className='flex flex-column items-center'>
+			<div className='tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-5 pointer'>
+				<img alt="" src={props.img}/>
+				<h2>{props.name}</h2>
+			</div>
+			<button 
+				className='dim w-20 f4 link pa3 dib br3 black fw-4 bg-light-yellow' 
+				// onClick={onSubmit}
+			>Add Pokemon
+			</button>
+
+		</div>
+	)
+
 }
 
 
--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Card from './Card';
 
-function CardList({pokemons}) {
+function CardList({pokemons, currentPage}) {
 	return (
 		<div>
 			{pokemons.map((user, index) => 
-				<Card key={pokemons[index].id} name={pokemons[index].name} 
-					img ={pokemons[index].img}/>
+				<Card 
+					key={pokemons[index].id} 
+					name={pokemons[index].name} 
+					img ={pokemons[index].img}
+					currentPage={currentPage}
+				/>
 			)}
 		</div>
 	);
