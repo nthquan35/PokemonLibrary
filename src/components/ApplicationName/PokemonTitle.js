e4edbe85b96a3c9b3b8c794e4bdd6f0b95d72e0e
Quan <hongquan03051997@gmail.com>
2021-10-18 12:00:28 -0700

    refractor folder

--- src/components/ApplicationName/PokemonTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './PokemonTitle.css';
-
-const PokemonTitle = () => {
-	return(
-		<h1 className= 'f-headline ma2'>Pokemon Index</h1>
-)}
-
-export default PokemonTitle;
